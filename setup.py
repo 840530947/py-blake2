dfa85f30b9e7ae96fb8e201717892fd6f1792975
Lukáš <lukaszahradnik96@seznam.cz>
2018-05-28 02:52:46 +0200

    Compare outputs with standard blake2b function

--- setup.py
+++ setup.py
@@ -1,5 +1,10 @@
 from src.blake2 import Blake2b
+import hashlib
 
 blake = Blake2b()
 
-print(blake.getHash("hello world", "key"))
+bb = hashlib.blake2b(b'input').hexdigest()
+print(bb)
+
+res = blake.getHash(b'input')
+print(res)
